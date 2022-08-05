a6a1d7bb851d53983ac211faa11a9e784f052a58
MunroAlvin <munroalvin@gmail.com>
2022-08-05 17:09:04 +0300

    Add - Date(), getDay()

--- app.js
+++ app.js
@@ -7,14 +7,26 @@ const port = 3000;
 
 // bodyParser initilization.
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
 
-
+    const currentDate = new Date();
+    const today = currentDate.getDay();
+    if (today == 6 || today == 0) {
+        console.log("Today is weekend");
+        res.write("Today is weekend") // this sends a chunk of the response body. This method may be called multiple times to provide successive parts of the body.
+    } else {
+        console.log("Today is week day");
+        res.write("Today is week day");
+        res.write("another message");
+        res.send();
+    }
 });
 
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
